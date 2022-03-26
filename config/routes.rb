ad2e71455a1136225eabc9de9255c2011d9c8933
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-26 18:44:12 -0500

    Update routes for api v2

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     namespace :v1 do 
       resources :books, only: [:index, :show, :create, :update, :destroy]
     end
+    namespace :v2 do 
+      resources :books, only: [:index]
+    end
   end
   
 end
