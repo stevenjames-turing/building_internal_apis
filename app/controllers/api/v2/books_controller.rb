330b73f53dfac0f8c9f05ab3b473c607b740e272
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-26 18:47:38 -0500

    Add serialized for API v2

--- app/controllers/api/v2/books_controller.rb
+++ app/controllers/api/v2/books_controller.rb
@@ -1,6 +1,7 @@
 class Api::V2::BooksController < ApplicationController
   
   def index 
-    render json: Book.all
+    books = Book.all
+    render json: BookSerializer.format_books(books)
   end
 end
