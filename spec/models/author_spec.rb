da5e693d596a840490c64259bd39883e05b949dd
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-27 11:38:34 -0500

    Test: Author model relationships

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe Author, type: :model do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
   end
-  
+  describe 'relationships' do 
+    it { should have_many :books }
+  end
 end
