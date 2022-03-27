a7f20e8bd5bd76d361d30f794a8943f5dd99da4f
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-27 11:04:05 -0500

    Reference Author on Books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_27_155929) do
+ActiveRecord::Schema.define(version: 2022_03_27_160322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 2022_03_27_155929) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "popularity"
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  add_foreign_key "books", "authors"
 end
