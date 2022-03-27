5003e3b579a6ae0fbfec4a6ba3f1a63362da8801
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-27 11:35:27 -0500

    Book belongs to Author

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,13 @@
+
 class Book < ApplicationRecord
+  # Model Validations
   validates :title, :genre, :summary, :popularity, presence: true
   validates :number_sold, presence: true, numericality: true 
   
+  # Model Relationships
+  belongs_to :author
+  
+  # Call_back for V2 API adding popularity to DB
   before_save { |book| book.popularity = calculate_popularity }
 
   private 
