dbf8c680ee6d0ddbc26fa2f153e3f97bbdd88a3c
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-27 11:01:09 -0500

    Remove author from Books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_27_155134) do
+ActiveRecord::Schema.define(version: 2022_03_27_155929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 2022_03_27_155134) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "author"
     t.string "genre"
     t.text "summary"
     t.integer "number_sold"
