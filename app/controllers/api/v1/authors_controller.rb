7513807d15ffcdd12ad7375047b386516135213a
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-27 12:33:44 -0500

    Feat: it can create a new author

--- app/controllers/api/v1/authors_controller.rb
+++ app/controllers/api/v1/authors_controller.rb
@@ -9,7 +9,15 @@ class Api::V1::AuthorsController < ApplicationController
     render json: @author
   end
 
+  def create 
+    render json: Author.create!(author_params)
+  end
+
   private 
+
+    def author_params 
+      params.require(:author).permit(:first_name, :last_name)
+    end
 
     def find_author
       @author = Author.find(params[:id])
